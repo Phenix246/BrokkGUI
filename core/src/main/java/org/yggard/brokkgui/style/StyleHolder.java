d5f618877eba6202d6e4a403421908450a04fbaa
Phenix246 <florian.gomes246@gmail.com>
2017-03-06 16:57:38 +0100

    Add a null check

--- core/src/main/java/org/yggard/brokkgui/style/StyleHolder.java
+++ core/src/main/java/org/yggard/brokkgui/style/StyleHolder.java
@@ -5,20 +5,20 @@ import fr.ourten.teabeans.value.MapProperty;
 
 public class StyleHolder
 {
-    private final IStyleable    parent;
-    MapProperty<String, Object> propreties;
+    private final IStyleable                  parent;
+    private final MapProperty<String, Object> propreties;
 
     public StyleHolder(IStyleable parent)
     {
         this.parent = parent;
-        propreties = new BaseMapProperty<>(null);
+        this.propreties = new BaseMapProperty<>(null);
     }
 
     public Object getStyle(String key)
     {
         if (this.propreties.containsKey(key))
             return this.propreties.get(key);
-        else if (this.parent != null)
+        else if (this.parent != null && this.parent.getStyleHolder() != null)
             return this.parent.getStyleHolder().getStyle(key);
         else
             return null;
